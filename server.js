b22c9303f7a432c2cd9fb0f05438282cd08c0033
bdalberson <Brian.alberson@gmail.com>
2023-04-26 17:01:52 -0700

    installed UUID

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const path = require("path");
-const port = 3001;
+const port = process.env.PORT || 3001
 const api = require('./routes/apiroutes.js');
 const util = require('util');
 app.use(express.urlencoded({ extended: true }));
@@ -19,6 +19,13 @@ app.get('/index', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.post("/api/notes/id"), (req,res) => {
+  const newNote = req.body;
+  readFile("db")
+  noteList.push(newNote)
+  res.json(newNote)
+}
 
 app.listen(port, () =>
   console.info(`Example app listening at http://localhost:${port} 🚀`)
